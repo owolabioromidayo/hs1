644cc1e9b7b63834872bc4a64ae021401cf1891a
Owolabi Oromidayo <owolabi.oromidayo@stu.cu.edu.ng>
2022-04-14 14:26:33 +0100

    Move from pipenv to venv, ML integration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
-import os, requests, sys
+import os, requests, sys, datetime
 
 from flask import Flask, render_template, request
-from pymongo import MongoClient
+import pymongo
 from dotenv import load_dotenv
+
+import joblib
+from sklearn.tree import DecisionTreeClassifier
+import numpy
+import pandas as pd
 
 load_dotenv()
 
@@ -37,17 +42,44 @@ def publish_sensors():
     #connect to mongodb
     db_name = os.environ.get('MONGO_DB_NAME', None)
     CONNECTION_STRING = os.environ.get("MONGO_CONNECTION_STRING", None)
-    print(CONNECTION_STRING)
-    client = MongoClient(CONNECTION_STRING)
+    client = pymongo.MongoClient(CONNECTION_STRING)
+    db = client[db_name]
     
-    mode = client[db_name]['config'].find_one({"key": "mode"})["value"] #fix this
+    mode = db['config'].find_one({"key": "mode"})["value"] 
     print(f"Current mode: {mode}")
 
     label = None
     
-    if mode == "regression_model":
-        pass
-        #get model file and use to predict
+    if mode == "machine_learning":  
+        last_update = db['config'].find_one({"key": "last_model_update_time"})["value"] #get last update timestamp -> from mongo
+
+        #retrain model if set time period has elapsed
+        training_freq = db['config'].find_one({"key": "training_freq"})["value"] 
+        if datetime.datetime.now() - last_update >= datetime.timedelta(days = training_freq):
+            print("Retraining model...")
+            ML_ENDPOINT = os.environ.get("ML_ENDPOINT")
+            requests.post(f"https://{ML_ENDPOINT}/train")
+        
+        curr_model = db['ml_test'].find().sort('datetime', pymongo.DESCENDING)[0] #get most recent model file ( sort by date)
+
+        #update stored model if not current
+        if last_update < curr_model['datetime']:
+            with open('model.pkl', 'wb') as f:
+                f.write(curr_model['file'])
+            
+            db['config'].update_one({"key": "last_model_update_time"}, { "$set": { 'value': datetime.datetime.now() } })
+        
+        loaded_model = joblib.load("model.pkl")
+
+        #make prediction and save to label
+        df = pd.DataFrame.from_dict({"baro_pressure": [json["baro_pressure"]],
+                                     "ext_temp": [json["ext_temp"]],
+                                     "humidity": [json["humidity"]], 
+                                     "wind_speed": [json["wind_speed"]],
+                                     "wind_direction": [json["wind_direction"]],                                 
+                                    })
+        label = loaded_model.predict(df)[0]
+
     elif mode == "weather_api":
         #use weather api
         lat = os.environ.get("LATITUDE", None)
@@ -55,17 +87,15 @@ def publish_sensors():
         api_key = os.environ.get("WEATHERBIT_API_KEY", None)
 
         weatherbit_request_string = f"https://api.weatherbit.io/v2.0/current?lat={lat}&lon={_long}&key={api_key}"
-
         r = requests.get(weatherbit_request_string)
         label = r.json()["data"][0]["weather"]["description"]
-
     else:
         return "Failed! Operation mode not supported."
 
 
     #save labelled data to db
     json["label"] = label
-    client[db_name]["weather_data"].insert_one(json)
+    db["weather_data"].insert_one(json)
 
     return "Successful!"
 
